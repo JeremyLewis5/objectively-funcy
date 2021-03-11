15c1f1f9953463d61bff9947631aaaed97cd8975
JeremyLewis5 <jeremy.lewis@codeimmersives.com>
2021-03-11 15:44:07 -0600

    asdasdf

--- main.js
+++ main.js
@@ -12,29 +12,41 @@ function getFullName(user) {
   return fullName;
 }
  
-function setFirstName(user) {
+function setFirstName(user, newName) {
 //    const newName = user.firstName;
 //    if 
+   user.firstName = newName;
   }
 
-  function setAge(user) {
-
+  function setAge(user, Newage) {
+   user.age = Newage;
   }
 
 function giveBirthday(user) {
-  if (user.age === '') {
-    return user.age === 1;
-  } else {
+  if (user.age !== undefined) {
     return user.age ++;
+  } else {
+    return user.age = 1;
   }
 
   
 }
 
-function marry(user) {
-  
+function marry(user1, user2) {
+  user1.married = true;
+  user2.married= true;
+  user1.spouseName = getFullName(user2);
+  user2.spouseName = getFullName(user1);
 }
 
+function divorce(user1, user2) {
+  user1.married = false;
+  user2.married= false;
+  delete user1.spouseName;
+  delete user2.spouseName;
+  
+
+}
 
 
 
